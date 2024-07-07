f377f93cec3b2232a9b49846cd70587058b7db47
wmaiven <maivencunha@hotmail.com>
2024-07-07 12:12:13 -0300

    trazendo criando e colocando dados dinamics em grafico receita mes a mes e inadiplencia mes a mes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using TechNationFinanceiroClient;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddHttpClient<INotaFiscalService, NotaFiscalService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:32878/api/");
+    client.BaseAddress = new Uri("https://localhost:5001/api/");
 });
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
